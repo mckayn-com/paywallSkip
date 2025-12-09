1fe33379bd0541dfde36eb47b9868d1160e47953
Ankur Jaiswal <ankurjx10@gmail.com>
2025-12-09 14:38:54 +0530

    Update layout.tsx
    Updated the Ad code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,7 +80,7 @@ export default function RootLayout({
        <head>
         <Script
           async
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7474815095793448"
           crossOrigin="anonymous"
         />
