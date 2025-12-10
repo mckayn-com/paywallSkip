b13bad51aab5eb269cbbe90ff7f6c55c88886940
Ankur Jaiswal <ankurjx10@gmail.com>
2025-12-10 18:02:11 +0530

    Create AdsenseScript.tsx

---
+++ src/components/AdsenseScript.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+export default function ConditionalAdScript() {
+  const pathname = usePathname();
+
+  if (pathname.startsWith("/article")) {
+    return null;
+  }
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7474815095793448";
+    script.crossOrigin = "anonymous";
+    document.body.appendChild(script);
+
+    return () => {
+      script.remove();
+    };
+  }, []);
+
+  return null;
+}
