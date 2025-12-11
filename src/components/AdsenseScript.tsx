3cbf970512b1ce7c29d6e9c2422c5a8902946ae7
Ankur Jaiswal <ankurjx10@gmail.com>
2025-12-11 13:08:26 +0530

    Update AdsenseScript.tsx

--- src/components/AdsenseScript.tsx
+++ src/components/AdsenseScript.tsx
@@ -6,22 +6,21 @@ import { useEffect } from "react";
 export default function ConditionalAdScript() {
   const pathname = usePathname();
 
-  if (pathname.startsWith("/article")) {
-    return null;
-  }
-
   useEffect(() => {
+    // Skip script injection on article pages
+    if (pathname.startsWith("/article")) return;
+
     const script = document.createElement("script");
     script.async = true;
     script.src =
       "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7474815095793448";
-    script.crossOrigin = "anonymous23";
+    script.crossOrigin = "anonymous";
     document.body.appendChild(script);
 
     return () => {
       script.remove();
     };
-  }, []);
+  }, [pathname]);
 
   return null;
 }
