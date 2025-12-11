60ef334d90de88af1235fa60069c799b0f0c6f73
Ankur Jaiswal <ankurjx10@gmail.com>
2025-12-11 13:09:08 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,11 +83,7 @@ export default function RootLayout({
       <body className={karla.className}>
         {children}
         <Toaster />
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7474815095793448"
-          crossOrigin="anonymous"
-        />
+                <AdsenseScript />
         <Script id="schema-org" type="application/ld+json">
           {`
             {
